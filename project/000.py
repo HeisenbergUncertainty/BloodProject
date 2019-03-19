02f999e5937415149fdda4ff5df6ff86710750e3
HeisenbergUncertainty <44612138+HeisenbergUncertainty@users.noreply.github.com>
2019-03-19 17:05:33 +0300

    Add files via upload

--- project/000.py
+++ project/000.py
@@ -22,6 +22,29 @@ class AddNewsForm(FlaskForm):
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 db = DB()
+
+
+@app.route('/')
+def start_screen():
+    return redirect("/registration")
+
+@app.route('/registration', methods=['GET', 'POST'])
+def registration():
+    form = LoginForm()
+    user_name = form.username.data
+    password = form.password.data
+    user_model = UserModel(db.get_connection())
+    exists = user_model.exists(user_name, password)
+    if exists[0]:
+        user_model.insert(user_name, password)
+        exists = user_model.exists(user_name, password)
+        session['username'] = user_name
+        session['user_id'] = exists[1]
+        return redirect("/news")
+    else:
+        pass #Error пользователь уже есть
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -34,9 +57,20 @@ def login():
         session['user_id'] = exists[1]
         return redirect("/news")
     else:
-        user_model.insert(user_name, password)
-        return render_template('login.html', title='Авторизация', form=form)
+        pass #Error пользователя несуществует
 
+@app.route('/logout')
+def logout():
+    session.pop('username', 0)
+    session.pop('user_id', 0)
+    return redirect('/login')
+
+@app.route('/news', methods=['GET'])
+def news():
+    if 'username' not in session:
+        return redirect('/login')
+    nm = NewsModel(db.get_connection()).get_all()
+    return render_template('news.html', news=nm)
 
 @app.route('/add_news', methods=['GET', 'POST'])
 def add_news():
@@ -52,7 +86,6 @@ def add_news():
     return render_template('add_news.html', title='Добавление новости',
                            form=form, username=session['username'])
 
-
 @app.route('/delete_news/<int:news_id>', methods=['GET'])
 def delete_news(news_id):
     if 'username' not in session:
@@ -61,19 +94,7 @@ def delete_news(news_id):
     nm.delete(news_id)
     return redirect("/news")
 
-@app.route('/logout')
-def logout():
-    session.pop('username', 0)
-    session.pop('user_id', 0)
-    return redirect('/login')
 
-
-@app.route('/news', methods=['GET'])
-def news():
-    if 'username' not in session:
-        return redirect('/login')
-    nm = NewsModel(db.get_connection()).get_all()
-    return render_template('news.html', news=nm)
 
 if __name__ == '__main__':
     app.config['SECRET_KEY'] = 'secret_key'
